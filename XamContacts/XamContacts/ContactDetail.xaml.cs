ca90e39305175ebbb959ae53370f09cfd1d729a3
honzikprevratil <honzikprevratil@gmail.com>
2017-11-29 18:03:42 +0100

    Clicking list

---
+++ XamContacts/XamContacts/ContactDetail.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamContacts
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ContactDetail : ContentPage
+    {
+        public ContactDetail()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ContentPage_BindingContextChanged(object sender, EventArgs e)
+        {
+            contactImage.Source = ((Contact)sender).Image;
+            contactName.Text = ((Contact)sender).DisplayName;
+        }
+    }
+}
--- XamContacts/XamContacts/MainPage.xaml.cs
+++ XamContacts/XamContacts/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,25 @@ namespace XamContacts
 {
     public partial class MainPage : ContentPage
     {
+        private ObservableCollection<Contact> contacts = new ObservableCollection<Contact>();
+        public ContactDetail contactDetail = new ContactDetail();
         public MainPage()
         {
             InitializeComponent();
+            //defined in XAML to follow
+            ContacsView.ItemsSource = contacts;
+            contacts.Add(new Contact { DisplayName = "Rob Finnerty", Image = "avatar.png" });
+            contacts.Add(new Contact { DisplayName = "Bill Wrestler", Image = "avatar.png" });
+            contacts.Add(new Contact { DisplayName = "Dr. Geri-Beth Hooper", Image = "avatar.png" });
+            contacts.Add(new Contact { DisplayName = "Dr. Keith Joyce-Purdy", Image = "avatar.png" });
+            contacts.Add(new Contact { DisplayName = "Sheri Spruce", Image = "avatar.png" });
+            contacts.Add(new Contact { DisplayName = "Burt Indybrick", Image = "avatar.png" });
+        }
+
+        private void ContacsView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            App.Current.BindingContext = sender;
+            Navigation.PushModalAsync(contactDetail);
         }
     }
 }
