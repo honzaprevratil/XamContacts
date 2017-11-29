2968d8bebf47160f131246ab22ebcbe0927bb812
honzikprevratil <honzikprevratil@gmail.com>
2017-11-29 21:08:43 +0100

    android load test

---
+++ XamContacts/XamContacts.Android/ContactService_Android.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Provider;
+
+namespace XamContacts.Droid
+{
+    public class ContactService_Android
+    {
+        public IEnumerable<PhoneContact> GetAllContacts()
+        {
+            var phoneContacts = new List<PhoneContact>();
+
+            using (var phones = Android.App.Application.Context.ContentResolver.Query(ContactsContract.CommonDataKinds.Phone.ContentUri, null, null, null, null))
+            {
+                if (phones != null)
+                {
+                    while (phones.MoveToNext())
+                    {
+                        try
+                        {
+                            string name = phones.GetString(phones.GetColumnIndex(ContactsContract.Contacts.InterfaceConsts.DisplayName));
+                            string phoneNumber = phones.GetString(phones.GetColumnIndex(ContactsContract.CommonDataKinds.Phone.Number));
+
+                            string[] words = name.Split(' ');
+                            var contact = new PhoneContact();
+                            contact.FirstName = words[0];
+                            if (words.Length > 1)
+                                contact.LastName = words[1];
+                            else
+                                contact.LastName = ""; //no last name
+                            contact.PhoneNumber = phoneNumber;
+                            phoneContacts.Add(contact);
+                        }
+                        catch (Exception ex)
+                        {
+                            //something wrong with one contact, may be display name is completely empty, decide what to do
+                        }
+                    }
+                    phones.Close();
+                }
+                // if we get here, we can't access the contacts. Consider throwing an exception to display to the user
+            }
+
+            return phoneContacts;
+        }
+    }
+}
